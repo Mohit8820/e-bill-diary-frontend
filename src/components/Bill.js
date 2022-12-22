30858b94bd5e9cbdb45ec23837c17a239970382e
Mohit8820 <mohitprasadsah8820@gmail.com>
2022-12-23 02:24:30 +0530

    bill page

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -28,7 +28,17 @@ const Bill = () => {
   useEffect(() => {
     const generateQR = async (text) => {
       try {
-        const qrSrc = await QRCode.toDataURL(JSON.stringify(location.state));
+        const qrSrc = await QRCode.toDataURL(
+          JSON.stringify({
+            Name: user.name,
+            Bill_id: user.lastBill.id,
+            Date_generated: user.lastBill.dateGenerated,
+            Date_paid: user.lastBill.datePaid,
+            Reading: user.lastBill.Reading,
+            Amount: user.lastBill.Amount,
+            Status: user.lastBill.Status,
+          })
+        );
         setSrc(qrSrc);
       } catch (err) {
         console.error(err);
@@ -40,74 +50,148 @@ const Bill = () => {
   if (auth.isLoggedIn)
     return (
       <div className="generate">
-        <span className="my-badge">Bill</span>
+        <h2>Bill</h2>
         <div className="bill">
+          <div className={`${user.lastBill.Status} status`}>
+            {user.lastBill.Status === "Paid" ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+                stroke-linejoin="round"
+                stroke-miterlimit="2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.959 17l-4.5-4.319 1.395-1.435 3.08 2.937 7.021-7.183 1.422 1.409-8.418 8.591z" />
+              </svg>
+            ) : user.lastBill.Status === "Due" ? (
+              <svg
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+                stroke-linejoin="round"
+                stroke-miterlimit="2"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="m12.002 21.534c5.518 0 9.998-4.48 9.998-9.998s-4.48-9.997-9.998-9.997c-5.517 0-9.997 4.479-9.997 9.997s4.48 9.998 9.997 9.998zm0-1.5c-4.69 0-8.497-3.808-8.497-8.498s3.807-8.497 8.497-8.497 8.498 3.807 8.498 8.497-3.808 8.498-8.498 8.498zm0-6.5c-.414 0-.75-.336-.75-.75v-5.5c0-.414.336-.75.75-.75s.75.336.75.75v5.5c0 .414-.336.75-.75.75zm-.002 3c.552 0 1-.448 1-1s-.448-1-1-1-1 .448-1 1 .448 1 1 1z"
+                  fill-rule="nonzero"
+                />
+              </svg>
+            ) : (
+              <svg
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+                stroke-linejoin="round"
+                stroke-miterlimit="2"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="m21.897 13.404.008-.057v.002c.024-.178.044-.357.058-.537.024-.302-.189-.811-.749-.811-.391 0-.715.3-.747.69-.018.221-.044.44-.078.656-.645 4.051-4.158 7.153-8.391 7.153-3.037 0-5.704-1.597-7.206-3.995l1.991-.005c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-4.033c-.414 0-.75.336-.75.75v4.049c0 .414.336.75.75.75s.75-.335.75-.75l.003-2.525c1.765 2.836 4.911 4.726 8.495 4.726 5.042 0 9.217-3.741 9.899-8.596zm-19.774-2.974-.009.056v-.002c-.035.233-.063.469-.082.708-.024.302.189.811.749.811.391 0 .715-.3.747-.69.022-.28.058-.556.107-.827.716-3.968 4.189-6.982 8.362-6.982 3.037 0 5.704 1.597 7.206 3.995l-1.991.005c-.414 0-.75.336-.75.75s.336.75.75.75h4.033c.414 0 .75-.336.75-.75v-4.049c0-.414-.336-.75-.75-.75s-.75.335-.75.75l-.003 2.525c-1.765-2.836-4.911-4.726-8.495-4.726-4.984 0-9.12 3.654-9.874 8.426z"
+                  fill-rule="nonzero"
+                />
+              </svg>
+            )}
+            {user.lastBill.Status}
+          </div>
+          <div className="bill-head">
+            <img src={src} alt="qr code" className="qr" />
+            <p>{user.name}</p>
+          </div>
           <table className="table">
-            {/* <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead> */}
-            <tbody>
+            <tr>
+              <th>Bill date</th>
+              <td>{billdate}</td>
+            </tr>
+            <tr>
+              <th>Bill Amount</th>
+              <td>{user.lastBill.Amount}</td>
+            </tr>
+            <tr>
+              <th>Previous reading</th>
+              <td>{user.prevReading}</td>
+            </tr>
+            <tr>
+              <th>Current Reading</th>
+              <td>{user.lastBill.Reading}</td>
+            </tr>
+
+            {user.lastBill.datePaid !== null &&
+              user.lastBill.Status === "Paid" && (
+                <tr>
+                  <th>Bill Paid on</th>
+                  <td>{styledDate(user.lastBill.datePaid)}</td>
+                </tr>
+              )}
+            {user.lastBill.Note !== null && user.lastBill.Note && (
               <tr>
-                <th scope="row">Name</th>
-                <td>{user.name}</td>
+                <th>Note</th>
+                <td>{user.lastBill.Note}</td>
               </tr>
-              <tr>
-                <th scope="row">Bill date</th>
-                <td>{billdate}</td>
-              </tr>
-              <tr>
-                <th scope="row">Previous reading</th>
-                <td>{user.prevReading}</td>
-              </tr>
-              <tr>
-                <th scope="row">Current Reading</th>
-                <td>{user.lastBill.Reading}</td>
-              </tr>
-              <tr>
-                <th scope="row">Bill Amount</th>
-                <td>{user.lastBill.Amount}</td>
-              </tr>
-              <tr>
-                <th scope="row">Bill Status</th>
-                <td className={user.lastBill.Status}>{user.lastBill.Status}</td>
-              </tr>
-              {user.lastBill.datePaid !== null &&
-                user.lastBill.Status === "Paid" && (
-                  <tr>
-                    <th scope="row">Bill Paid on</th>
-                    <td>{styledDate(user.lastBill.datePaid)}</td>
-                  </tr>
-                )}
-            </tbody>
+            )}
           </table>
         </div>
-        <div className="payment">
-          <figcaption className="blockquote-footer">
-            To verify - Scan the QR code
-          </figcaption>
-          <img src={src} alt="qr code" className="qr" />
-        </div>
+
         <div className="btn-flex">
           <style>{`@media print {.btn-flex{display: none;}}`}</style>
           {user.lastBill.Status === "Due" && (
-            <button
-              type="button"
-              className="btn btn-lg btn-primary"
-              onClick={sendToPayment}
-            >
+            <button className="success-btn" onClick={sendToPayment}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="2.3rem"
+                height="1.7rem"
+                viewBox="0 0 23 17"
+                fill="none"
+              >
+                <path
+                  d="M19.75 1H2.875C1.83947 1 1 1.83947 1 2.875V14.125C1 15.1605 1.83947 16 2.875 16H19.75C20.7855 16 21.625 15.1605 21.625 14.125V2.875C21.625 1.83947 20.7855 1 19.75 1Z"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M1 6.625H21.625"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>{" "}
               Pay
             </button>
           )}
-          <button
-            type="button"
-            className="btn btn-lg btn-success"
-            onClick={print}
-          >
+          <button className="primary-btn" onClick={print}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.9rem"
+              height="1.9rem"
+              viewBox="0 0 19 19"
+              fill="none"
+            >
+              <path
+                d="M4.3999 6.95V1H14.5999V6.95"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M4.4 14.6H2.7C2.24913 14.6 1.81673 14.4208 1.49792 14.102C1.17911 13.7832 1 13.3508 1 12.9V8.64995C1 8.19908 1.17911 7.76668 1.49792 7.44787C1.81673 7.12906 2.24913 6.94995 2.7 6.94995H16.3C16.7509 6.94995 17.1833 7.12906 17.5021 7.44787C17.8209 7.76668 18 8.19908 18 8.64995V12.9C18 13.3508 17.8209 13.7832 17.5021 14.102C17.1833 14.4208 16.7509 14.6 16.3 14.6H14.6"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M14.5999 11.2H4.3999V18H14.5999V11.2Z"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>{" "}
             Print
           </button>
         </div>
--- src/components/History.js
+++ src/components/History.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 import { AuthContext } from "../contexts/auth-context";
-import { styledDate } from "../assets/styledDate";
+import { styledDate, getDay, getMonth, getYear } from "../assets/styledDate";
 import { useHttpClient } from "../hooks/http-hook";
 import Modal from "./UIElements/Modal";
 import ErrorModal from "../components/UIElements/ErrorModal";
@@ -177,9 +177,150 @@ const History = () => {
 
       <div className="home">
         {isLoading && <LoadingSpinner asOverlay />}
-        <span className="my-badge">Your Bill History</span>
+        <h2>History</h2>
 
-        <div className="bill">
+        <div className="bill-list">
+          {user.history
+            .slice(0)
+            .reverse()
+            .map((bill, index) => (
+              <React.Fragment key={index}>
+                <div className="bill-item">
+                  <div className="date">
+                    {getMonth(bill.dateGenerated)}
+                    <p>{getDay(bill.dateGenerated)}</p>
+                    {getYear(bill.dateGenerated)}
+                  </div>
+                  <div className="col1">
+                    {billDate[user.history.length - 1 - index]}
+                  </div>
+                  <div className="col2">{bill.Reading}</div>
+                  <div className="col3">{bill.Amount}</div>
+                  {auth.userId !== process.env.REACT_APP_ADMIN_ID && (
+                    <div className={`col4 ${bill.Status}`}>
+                      {bill.Status}
+                      <br />
+                      {bill.Status === "Paid" && (
+                        <span>{styledDate(bill.datePaid)}</span>
+                      )}
+                    </div>
+                  )}
+                  {auth.userId === process.env.REACT_APP_ADMIN_ID && (
+                    <div className="btn-group col4">
+                      <button
+                        className="btn update-btn"
+                        type="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                        style={{
+                          backgroundColor:
+                            bill.Status === "Paid"
+                              ? "#d1e7dd"
+                              : bill.Status === "Due"
+                              ? "#f8d7da"
+                              : "#fff3cd",
+                        }}
+                      >
+                        {bill.Status}{" "}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-caret-down-fill"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                        </svg>
+                        <br />
+                        {bill.Status === "Paid" && (
+                          <span>{styledDate(bill.datePaid)}</span>
+                        )}
+                      </button>
+                      <ul className="dropdown-menu">
+                        <li>
+                          <button
+                            type="button"
+                            className="btn btn-danger"
+                            onClick={() => {
+                              setUpdateModal(true);
+                              setStatusBody({
+                                bill_id: bill.id,
+                                status: "Due",
+                                userId: auth.userId,
+                              });
+                            }}
+                          >
+                            Due
+                          </button>
+                        </li>
+                        <li>
+                          <button
+                            className="btn btn-warning"
+                            onClick={() => {
+                              setUpdateModal(true);
+                              setStatusBody({
+                                bill_id: bill.id,
+                                status: "Processing",
+                                userId: auth.userId,
+                              });
+                            }}
+                          >
+                            Processing
+                          </button>
+                        </li>
+                        <li>
+                          <button
+                            className="btn btn-success"
+                            onClick={() => {
+                              setUpdateModal(true);
+                              setStatusBody({
+                                bill_id: bill.id,
+                                status: "Paid",
+                                userId: auth.userId,
+                                datePaid: new Date(),
+                              });
+                            }}
+                          >
+                            Paid
+                          </button>
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+                  <div className="col5">{bill.Note}</div>
+                  <button
+                    className="btn btn-outline-primary view-btn"
+                    onClick={() => openBill(user.history.length - 1 - index)}
+                  >
+                    view
+                  </button>
+                  {auth.userId === process.env.REACT_APP_ADMIN_ID && (
+                    <button
+                      className="btn delete-btn"
+                      onClick={() => {
+                        setBillId(bill.id);
+                        setDeleteBill(true);
+                      }}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-trash-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                      </svg>
+                    </button>
+                  )}
+                </div>
+              </React.Fragment>
+            ))}
+        </div>
+
+        <div className="bill" style={{ display: "none" }}>
           <p className="lead" id="lead">
             Name : <strong>{user.name}</strong>
           </p>
@@ -252,7 +393,6 @@ const History = () => {
                           </button>
                           <ul className="dropdown-menu">
                             <li>
-                              {" "}
                               <button
                                 type="button"
                                 className="btn btn-danger"
